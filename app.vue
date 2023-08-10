a7ac0ae637330e771a8981cbe966baf22ed45971
NichiNect <yoniwidhi2@gmail.com>
2023-08-10 21:07:41 +0700

    Install tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <NuxtWelcome />
+    <div class="flex p-4 bg-sky-500">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nesciunt, dignissimos minima voluptate quaerat dolore accusamus rem, ipsam quod provident placeat eum! Porro voluptates earum architecto rem aspernatur blanditiis consequatur.
+    </div>
   </div>
 </template>
