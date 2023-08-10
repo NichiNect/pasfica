32babccba4d38a3fb57ea94213ca50f6a1a2a698
NichiNect <yoniwidhi2@gmail.com>
2023-08-10 21:39:28 +0700

    Install fontawesome

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <NuxtWelcome />
-    <div class="flex p-4 bg-sky-500">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nesciunt, dignissimos minima voluptate quaerat dolore accusamus rem, ipsam quod provident placeat eum! Porro voluptates earum architecto rem aspernatur blanditiis consequatur.
+    <div class="flex flex-col p-4 bg-sky-500">
+      <FontAwesomeIcon icon="fas fa-search" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore nesciunt, dignissimos minima voluptate quaerat dolore accusamus rem, ipsam quod provident placeat eum! Porro voluptates earum architecto rem aspernatur blanditiis consequatur.
+      </p>
     </div>
   </div>
 </template>
