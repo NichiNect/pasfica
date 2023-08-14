9aeb65a0b177162bab83c08323b0f69417753473
NichiNect <yoniwidhi2@gmail.com>
2023-08-14 13:36:53 +0700

    Modal side component with example

--- pages/admin/modal.vue
+++ pages/admin/modal.vue
@@ -11,6 +11,7 @@
  */
 const modalDefaultShow = ref(false);
 const modalConfirmShow = ref(false);
+const modalSideShow = ref(false);
 
 /**
  * * Methods
@@ -24,6 +25,9 @@ const modalConfirmHandler = () => {
 const modalConfirmSubmitHandler = (e) => {
   console.log('Modal onSubmit Clicked!!');
   modalConfirmShow.value = false;
+}
+const modalSideHandler = () => {
+  modalSideShow.value = true;
 }
 </script>
 
@@ -64,5 +68,28 @@ const modalConfirmSubmitHandler = (e) => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, expedita cumque. Enim rerum quidem odit. Minus, cumque quidem vitae dolores iusto corporis sed nihil facilis reprehenderit, eos voluptate perspiciatis accusamus?</p>
       </template>
     </BaseModalConfirm>
+
+    <button class="py-4 px-3 bg-primary hover:bg-opacity-70 text-white rounded" @click="modalSideHandler">Modal Side</button>
+    <BaseModalSide
+      :show="modalSideShow"
+      width="40%"
+      @onClose="() => modalSideShow = false"
+    >
+      <template #title>
+          <h6 class='text-xl font-semibold text-gray-600'>Title Modal Side</h6>
+          <p class='text-md text-gray-400 leading-4 mt-1'>Lorem ipsum dolor sit amet consectetur.</p>
+        </template>
+
+        <template #content>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, expedita cumque. Enim rerum quidem odit. Minus, cumque quidem vitae dolores iusto corporis sed nihil facilis reprehenderit, eos voluptate perspiciatis accusamus?</p>
+        </template>
+
+        <template #footer>
+          <div class='flex justify-end gap-4'>
+            <button class="py-2 px-3 bg-danger hover:opacity-70 text-white rounded">No</button>
+            <button class="py-2 px-3 bg-primary hover:opacity-70 text-white rounded">Yes</button>
+          </div>
+        </template>
+    </BaseModalSide>
   </div>
 </template>
