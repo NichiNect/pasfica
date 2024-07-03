d69a02a520e7292ae128edcaf8c1a2c8355015b5
NichiNect <yoniwidhi2@gmail.com>
2024-07-03 16:30:56 +0700

    Upgrade to nuxt 3.12.x

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
     'nuxt-swiper'
   ],
+
   plugins: [
     { src: '~/plugins/vcalendar.js', mode: 'client' },
   ],
+
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css',
     '~/assets/css/general.css',
     '~/assets/css/table.css',
   ],
+
   runtimeConfig: {
     public: {
       baseAPI: process.env.API_BASE_URL,
       baseURL: process.env.APP_URL,
     },
   },
-  ssr: true
+
+  ssr: true,
+  compatibilityDate: '2024-07-03'
 })
